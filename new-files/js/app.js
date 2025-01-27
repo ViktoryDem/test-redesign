787c371694820b2cc1a526640104e39d67727d13
ViktoryDem <36764620+ViktoryDem@users.noreply.github.com>
2025-01-27 21:05:01 +0200

    Delete new-files directory

--- new-files/js/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-document.addEventListener("DOMContentLoaded", function () {
-  const root = document.documentElement;
-
-  root.style.setProperty("--main-color", mainColor);
-  root.style.setProperty("--accent-color", accentColor);
-  root.style.setProperty("--secondary-color", secondaryColor);
-});
